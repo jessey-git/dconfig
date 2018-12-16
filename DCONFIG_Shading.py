69c42f308bcf67daf9e8f0b6594c53d04232267f
Jesse Y <jesse.y@gmail.com>
2018-12-16 02:46:21 -0800

    Use passed in context rather than bpy.context in remaining places

--- DCONFIG_Shading.py
+++ DCONFIG_Shading.py
@@ -21,16 +21,16 @@ class DC_OT_setup_shading(bpy.types.Operator):
     def execute(self, context):
         DC.trace_enter("DC_OT_setup_shading.execute")
 
-        bpy.context.space_data.shading.light = 'MATCAP'
-        bpy.context.space_data.shading.show_shadows = True
-        bpy.context.space_data.shading.show_cavity = True
-        bpy.context.space_data.shading.cavity_ridge_factor = 0
-        bpy.context.space_data.shading.cavity_valley_factor = 2
-        bpy.context.space_data.shading.curvature_ridge_factor = 0
-        bpy.context.space_data.shading.curvature_valley_factor = 2
-        bpy.context.space_data.shading.xray_alpha_wireframe = 0
+        context.space_data.shading.light = 'MATCAP'
+        context.space_data.shading.show_shadows = True
+        context.space_data.shading.show_cavity = True
+        context.space_data.shading.cavity_ridge_factor = 0
+        context.space_data.shading.cavity_valley_factor = 2
+        context.space_data.shading.curvature_ridge_factor = 0
+        context.space_data.shading.curvature_valley_factor = 2
+        context.space_data.shading.xray_alpha_wireframe = 0
 
-        bpy.context.space_data.overlay.wireframe_threshold = 0.86
+        context.space_data.overlay.wireframe_threshold = 0.86
 
         return DC.trace_exit("DC_OT_setup_shading.execute")
 
