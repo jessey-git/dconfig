39e6c31ce673f6ab4c14fcc0b713731dd0b42dc3
Jesse Y <jesse.y@gmail.com>
2020-02-20 01:20:47 -0800

    Add quick panel op

--- DCONFIG_Mesh.py
+++ DCONFIG_Mesh.py
@@ -30,13 +30,15 @@ class DCONFIG_MT_quick(bpy.types.Menu):
             dc.setup_op(layout, "mesh.remove_doubles", text="Weld vertices")
 
             layout.separator()
-            dc.setup_op(layout, "mesh.select_face_by_sides", text="Select N-Gons", type='GREATER', number=4, extend=False)
+            dc.setup_op(layout, "mesh.select_face_by_sides", text="Select N-Gons", type='NOTEQUAL', number=4, extend=False)
+            dc.setup_op(layout, "mesh.region_to_loop", text="Select Boundary Loop")
 
             layout.separator()
             layout.operator_context = 'INVOKE_REGION_WIN'
             dc.setup_op(layout, "mesh.fill_grid", text="Fill Grid")
             dc.setup_op(layout, "dconfig.make_quads", text="Make Quads")
             dc.setup_op(layout, "dconfig.subdivide_cylinder", text="Subdivide Cylinder")
+            dc.setup_op(layout, "dconfig.quick_panel", text="Quick Panel")
             dc.setup_op(layout, "dconfig.subd_bevel", text="Sub-D Bevel")
 
 
@@ -136,6 +138,33 @@ class DCONFIG_OT_subd_toggle(bpy.types.Operator):
                 mod_subd.show_viewport = True
             else:
                 mod_subd.show_viewport = not mod_subd.show_viewport
+
+        return dc.trace_exit(self)
+
+
+class DCONFIG_OT_quick_panel(bpy.types.Operator):
+    bl_idname = "dconfig.quick_panel"
+    bl_label = "DC Quick Panel"
+    bl_description = "Panel macro"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    offset: bpy.props.FloatProperty(name="Offset", default=0.0100, step=1, min=0.0001, max=1, precision=4)
+    offset2: bpy.props.FloatProperty(name="Offset (secondary)", default=0.0033, step=1, min=0.0001, max=1, precision=4)
+    inset: bpy.props.FloatProperty(name="Inset", default=0.0067, step=1, min=0.0001, max=1, precision=4)
+    depth: bpy.props.FloatProperty(name="Depth", default=0.0100, step=1, min=0.0001, max=1, precision=4)
+
+    @classmethod
+    def poll(cls, context):
+        return context.mode == 'EDIT_MESH' and dc.active_object_available(context, {'MESH'})
+
+    def execute(self, context):
+        dc.trace_enter(self)
+
+        bpy.ops.mesh.bevel(offset_type='OFFSET', offset=self.offset, offset_pct=0, segments=2, vertex_only=False)
+        bpy.ops.mesh.inset(thickness=self.inset, depth=-self.depth)
+        bpy.ops.mesh.select_more()
+        bpy.ops.mesh.region_to_loop()
+        bpy.ops.mesh.bevel(offset_type='OFFSET', offset=self.offset2, segments=2, profile=1, clamp_overlap=True, miter_outer='ARC')
 
         return dc.trace_exit(self)
 
